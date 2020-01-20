6687d5d18e0725761b8214016d9054a7e30a28bd
Frederic Ferrantls <f.ferrant@ephec.be>
2020-01-20 20:40:56 +0100

    Create PlusPetitPlusGrand

---
+++ Cours 01/src/exercices/PlusPetitPlusGrand.java
@@ -0,0 +1,22 @@
+package exercices;
+
+public class PlusPetitPlusGrand {
+    /*
+    Ecrivez un programme qui choisit un nombres entre 1 et 100, puis demande
+    à l'utilisateur de le deviner.
+    Si la proposition de l'utilisateur est mauvaise, le programme indique si
+    elle est trop grande ou trop petite, puis re-pose la question.
+
+    Une fois que l'utilisateur a deviné la réponde, le programme le félicite et
+    indique le nombre d'essais.
+
+
+
+    La méthode Math.random() retourne une valeur décomale comprise entre 0 et 1.
+    Pour obtenir la valeur entière d'un décimal, utilisez le cast: (int)valeur
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
